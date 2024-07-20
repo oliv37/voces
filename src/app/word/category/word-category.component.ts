34a53fabb8e9e25d4c16e3bd16cc97714b930f97
oliv37 <olivier.boisse@laposte.net>
2024-07-20 19:37:14 +0200

    feat: add es words

--- src/app/word/category/word-category.component.ts
+++ src/app/word/category/word-category.component.ts
@@ -1,6 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, HostListener, inject, input } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Word } from '../word.model';
-import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -8,5 +8,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './word-category.component.html',
 })
 export class WordCategoryComponent {
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+
   wordsInCategory = input.required<Word[]>();
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.router.navigate(['exercice'], { relativeTo: this.route });
+    }
+  }
 }
