fe6d2ad04e691b6d8423851894ce26a7d53359d2
oliv37 <olivier.boisse@laposte.net>
2024-10-19 16:25:53 +0200

    feat: rework mobile exercice page

--- src/app/directives/btn/btn.directive.ts
+++ src/app/directives/btn/btn.directive.ts
@@ -1,78 +1,71 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { computed, Directive, input } from '@angular/core';
+
+type Type = 'primary' | 'secondary';
+type Size = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+type Weight = 'none' | 'light' | 'normal' | 'bold';
+type Border = 'none' | 'normal' | 'large';
 
 @Directive({
   selector: '[appBtn]',
   standalone: true,
+  host: {
+    '[class]': 'class()',
+  },
 })
 export class BtnDirective {
-  @Input()
-  appBtnType: 'primary' | 'secondary' = 'primary';
+  appBtnTheme = input<Type>('primary');
+  appBtnTextSize = input<Size>('md');
+  appBtnFontWeight = input<Weight>('normal');
+  appBtnPadding = input<Size>('md');
+  appBtnBorder = input<Border>('normal');
 
-  @Input()
-  appBtnText: 'md' | 'xl' = 'md';
-
-  @Input()
-  appBtnFont: 'normal' | 'bold' = 'normal';
-
-  @Input()
-  appBtnPadding: 'none' | 'sm' | 'md' | 'lg' = 'md';
-
-  @HostBinding('class')
-  get class() {
-    return (
-      'flex justify-center items-center gap-1 ' +
-      'border border-black rounded-md ' +
-      'hover:opacity-80 disabled:opacity-40'
-    );
-  }
-
-  @HostBinding('class.bg-black')
-  @HostBinding('class.text-white')
-  get isPrimary() {
-    return this.appBtnType === 'primary';
-  }
-
-  @HostBinding('class.bg-white')
-  @HostBinding('class.text-black')
-  get isSecondary() {
-    return this.appBtnType === 'secondary';
-  }
-
-  @HostBinding('class.text-md')
-  get isTextMd() {
-    return this.appBtnText === 'md';
-  }
-
-  @HostBinding('class.text-xl')
-  get isTextXl() {
-    return this.appBtnText === 'xl';
-  }
-
-  @HostBinding('class.font-normal')
-  get isFontNormal() {
-    return this.appBtnFont === 'normal';
-  }
-
-  @HostBinding('class.font-bold')
-  get isFontBold() {
-    return this.appBtnFont === 'bold';
-  }
-
-  @HostBinding('class.px-2')
-  @HostBinding('class.py-1')
-  get isPaddingSm() {
-    return this.appBtnPadding === 'sm';
-  }
-
-  @HostBinding('class.px-3')
-  @HostBinding('class.py-2')
-  get isPaddingMd() {
-    return this.appBtnPadding === 'md';
-  }
-
-  @HostBinding('class.px-4')
-  @HostBinding('class.py-3')
-  get isPaddingLg() {
-    return this.appBtnPadding === 'lg';
-  }
+  class = computed<string>(() => {
+    return [
+      'flex justify-center items-center gap-1',
+      'hover:opacity-80 disabled:opacity-40',
+      CLASS_THEME[this.appBtnTheme()],
+      CLASS_TEXT_SIZE[this.appBtnTextSize()],
+      CLASS_FONT_WEIGHT[this.appBtnFontWeight()],
+      CLASS_PADDING[this.appBtnPadding()],
+      CLASS_BORDER[this.appBtnBorder()],
+    ]
+      .filter((s) => !!s)
+      .join(' ');
+  });
 }
+
+const CLASS_THEME: Record<Type, string> = {
+  primary: 'bg-black text-white',
+  secondary: 'bg-white text-black',
+};
+
+const CLASS_TEXT_SIZE: Record<Size, string> = {
+  none: '',
+  xs: 'text-xs',
+  sm: 'text-sm',
+  md: 'text-md',
+  lg: 'text-lg',
+  xl: 'text-xl',
+};
+
+const CLASS_PADDING: Record<Size, string> = {
+  none: '',
+  xs: 'px-1 py-0',
+  sm: 'px-2 py-1',
+  md: 'px-3 py-2',
+  lg: 'px-4 py-3',
+  xl: 'px-5 py-4',
+};
+
+const CLASS_FONT_WEIGHT: Record<Weight, string> = {
+  none: '',
+  light: 'font-light',
+  normal: 'font-normal',
+  bold: 'font-bold',
+};
+
+const CLASS_BORDER: Record<Border, string> = {
+  none: '',
+  normal: 'border border-black rounded-md',
+  large: 'border-2 border-black rounded-md',
+};
