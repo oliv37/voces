d31013c973f3d7fd07f4e45954014714c19fbf37
oliv37 <olivier.boisse@laposte.net>
2025-12-12 20:17:39 +0100

    feat: rework pagination bar

--- src/app/shared/bar/pagination-bar/pagination-bar.ts
+++ src/app/shared/bar/pagination-bar/pagination-bar.ts
@@ -5,46 +5,33 @@ import { Component, computed, input, output } from '@angular/core';
   templateUrl: './pagination-bar.html',
 })
 export class PaginationBar {
-  readonly totalPage = input.required<number>();
+  readonly lastPage = input.required<number>();
   readonly currentPage = input.required<number>();
   readonly highlightedPages = input<number[]>([]);
 
   readonly selectPage = output<number>();
 
-  protected readonly pages = computed<number[]>(() => {
-    const totalPage = this.totalPage();
-    const currentPage = this.currentPage();
-
-    const rangeSize = 5;
-    const rangePage = computeRangePage(currentPage, totalPage, rangeSize);
-    const startPage = rangePage.startPage;
-    const endPage = rangePage.endPage;
-    const pages = Array.from(
-      { length: endPage - startPage + 1 },
-      (_, i) => startPage + i
-    );
-
-    return [
-      ...(startPage > 1 ? [1] : []),
-      ...pages,
-      ...(endPage < totalPage ? [totalPage] : []),
-    ];
-  });
-}
-
-function computeRangePage(
-  currentPage: number,
-  totalPage: number,
-  rangeSize: number
-): { startPage: number; endPage: number } {
-  const startPage = Math.max(
-    1,
-    Math.min(
-      currentPage - Math.floor(rangeSize / 2),
-      totalPage - (rangeSize - 1)
-    )
+  protected readonly previousPage = computed<number>(() =>
+    Math.max(1, this.currentPage() - 1)
   );
-  const endPage = Math.min(totalPage, startPage + (rangeSize - 1));
 
-  return { startPage, endPage };
+  protected readonly nextPage = computed<number>(() =>
+    Math.min(this.lastPage(), this.currentPage() + 1)
+  );
+
+  protected readonly isFirstPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(1)
+  );
+
+  protected readonly isPreviousPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.previousPage())
+  );
+
+  protected readonly isNextPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.nextPage())
+  );
+
+  protected readonly isLastPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.lastPage())
+  );
 }
