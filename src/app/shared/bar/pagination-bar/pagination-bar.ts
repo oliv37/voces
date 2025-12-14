46af0cd43732cb78c548924d00f63366c66a557c
oliv37 <olivier.boisse@laposte.net>
2025-12-14 21:02:55 +0100

    feat: add pagination in word page

--- src/app/shared/bar/pagination-bar/pagination-bar.ts
+++ src/app/shared/bar/pagination-bar/pagination-bar.ts
@@ -1,4 +1,10 @@
-import { Component, computed, input, output } from '@angular/core';
+import {
+  booleanAttribute,
+  Component,
+  computed,
+  input,
+  output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pagination-bar',
@@ -7,31 +13,50 @@ import { Component, computed, input, output } from '@angular/core';
 export class PaginationBar {
   readonly lastPage = input.required<number>();
   readonly currentPage = input.required<number>();
+  readonly reversed = input<boolean, string>(false, {
+    transform: booleanAttribute,
+  });
   readonly highlightedPages = input<number[]>([]);
 
   readonly selectPage = output<number>();
 
-  protected readonly previousPage = computed<number>(() =>
-    Math.max(1, this.currentPage() - 1)
+  readonly upperLeftPage = computed<number>(() =>
+    this.reversed() ? this.lastPage() : 1
   );
 
-  protected readonly nextPage = computed<number>(() =>
-    Math.min(this.lastPage(), this.currentPage() + 1)
+  readonly upperRightPage = computed<number>(() =>
+    this.reversed() ? 1 : this.lastPage()
   );
 
-  protected readonly isFirstPageHighlighted = computed<boolean>(() =>
-    this.highlightedPages().includes(1)
+  protected readonly leftPage = computed<number>(() =>
+    this.reversed()
+      ? Math.min(this.upperLeftPage(), this.currentPage() + 1)
+      : Math.max(this.upperLeftPage(), this.currentPage() - 1)
   );
 
-  protected readonly isPreviousPageHighlighted = computed<boolean>(() =>
-    this.highlightedPages().includes(this.previousPage())
+  protected readonly rightPage = computed<number>(() =>
+    this.reversed()
+      ? Math.max(this.upperRightPage(), this.currentPage() - 1)
+      : Math.min(this.upperRightPage(), this.currentPage() + 1)
   );
 
-  protected readonly isNextPageHighlighted = computed<boolean>(() =>
-    this.highlightedPages().includes(this.nextPage())
+  protected readonly isCurrentPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.currentPage())
   );
 
-  protected readonly isLastPageHighlighted = computed<boolean>(() =>
-    this.highlightedPages().includes(this.lastPage())
+  protected readonly isUpperLeftPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.upperLeftPage())
+  );
+
+  protected readonly isUpperRightPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.upperRightPage())
+  );
+
+  protected readonly isLeftPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.leftPage())
+  );
+
+  protected readonly isRightPageHighlighted = computed<boolean>(() =>
+    this.highlightedPages().includes(this.rightPage())
   );
 }
--- src/app/word/word-page/word-page.ts
+++ src/app/word/word-page/word-page.ts
@@ -1,19 +1,43 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  input,
+  linkedSignal,
+} from '@angular/core';
 import type { OpenGraph } from '@shared/seo/open-graph.model';
 import { Meta } from '@shared/seo/meta';
 import type { WordGroup } from '../word.model';
 import { WordExerciceLink } from '../word-exercice/word-exercice-link/word-exercice-link';
+import { PaginationBar } from '@shared/bar/pagination-bar/pagination-bar';
+
+const NB_GROUPS_PER_PAGE = 30;
+let CURRENT_PAGE_SAVED: number | undefined;
 
 @Component({
-  imports: [Meta, WordExerciceLink],
+  imports: [Meta, WordExerciceLink, PaginationBar],
   templateUrl: './word-page.html',
 })
 export class WordPage {
   readonly wordGroups = input.required<WordGroup[]>();
 
-  protected readonly wordGroupsReversed = computed(() =>
-    this.wordGroups().slice().reverse()
+  protected readonly lastPage = computed(() =>
+    Math.ceil(this.wordGroups().length / NB_GROUPS_PER_PAGE)
   );
+
+  protected readonly currentPage = linkedSignal(() =>
+    this.#readSavedCurrentPage()
+  );
+
+  protected readonly wordGroupsToShow = computed(() => {
+    const currentPage = this.currentPage();
+    const wordGroups = this.wordGroups();
+
+    const startIdx = (currentPage - 1) * NB_GROUPS_PER_PAGE;
+    const endIdx = Math.min(startIdx + NB_GROUPS_PER_PAGE, wordGroups.length);
+
+    return wordGroups.slice(startIdx, endIdx).reverse();
+  });
 
   protected readonly nbWords = computed(
     () => this.wordGroups().flatMap((g) => g.words).length
@@ -27,4 +51,30 @@ export class WordPage {
     title: 'Voces - Vocabulaire Espagnol',
     description: `Voces | ${this.nbWords()} mots de Vocabulaire Espagnol`,
   }));
+
+  readonly saveCurrentPageEffect = effect(() => {
+    const currentPage = this.currentPage();
+    CURRENT_PAGE_SAVED = currentPage;
+  });
+
+  setCurrentPage(page: number) {
+    this.currentPage.set(page);
+    this.#scrollToTop();
+  }
+
+  #readSavedCurrentPage(): number {
+    const lastPage = this.lastPage();
+
+    const currentPage = Number(CURRENT_PAGE_SAVED);
+    const isValidCurrentPage =
+      !Number.isNaN(currentPage) && currentPage >= 1 && currentPage <= lastPage;
+
+    return isValidCurrentPage ? currentPage : lastPage;
+  }
+
+  #scrollToTop() {
+    if (typeof window !== 'undefined') {
+      window.scrollTo(0, 0);
+    }
+  }
 }
